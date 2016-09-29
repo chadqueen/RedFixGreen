ea4a8cad8cda6936fc1f7ffb266d13d8ffea3e57
chadqueen <chaddolan82@gmail.com>
2016-09-28 21:53:26 -0400

    Add NUnit and Moq

---
+++ src/RedFixGreen.Data/Country.cs
@@ -0,0 +1,13 @@
+﻿namespace RedFixGreen.Data
+{
+    public class Country
+    {
+        public Country(
+            string name)
+        {
+            this.Name = name;
+        }
+
+        public string Name { get; }
+    }
+}
---
+++ test/RedFixGreen.Data.Tests/CountryTests.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+
+namespace RedFixGreen.Data.Tests
+{
+    [TestFixture()]
+    public class CountryTests
+    {
+        [Test]
+        public void Name_Is_Immutable_Property()
+        {
+            // arrange
+            var countryName = "Alethkar";
+
+            // act
+            var country = new Country(countryName);
+
+            // assert
+            Assert.AreEqual(countryName, country.Name);
+        }
+    }
+}
